a1f8eb0581e365411867bf6443b1d41cf367fcba
Sahu <Avinash.Sahu@viacomcontractor.com>
2019-12-09 06:20:11 -0500

    commit

---
+++ AngularWebAPI/Startup.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+namespace AngularWebAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                    .AddJwtBearer(options =>
+                    {
+                        var serverSecret = new SymmetricSecurityKey(Encoding.UTF8.
+                     GetBytes(Configuration["JWT:key"]));
+                        options.TokenValidationParameters = new
+                       TokenValidationParameters
+                        {
+                            IssuerSigningKey = serverSecret,
+                            ValidIssuer = Configuration["JWT:Issuer"],
+                            ValidAudience = Configuration["JWT:Audience"]
+                        };
+                    });
+
+
+            services.AddMvc();
+            services.AddCors();
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer("Data Source=MSSQLD2008E531;Initial Catalog=soxworkflowdev;Persist Security Info=True;User ID=soxworkflowuser;Password=newuser123;"));
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+            });
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                //c.RoutePrefix = string.Empty;
+            });
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseCorsMiddleware();
+            app.UseAuthentication();
+            app.UseMvc();
+            app.UseCors("CorsPolicy");
+        }
+    }
+}
